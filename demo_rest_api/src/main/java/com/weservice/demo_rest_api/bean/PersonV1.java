975f410b1f5ab27a619d9786dd4d36bbd253e1f6
dipakdas99 <dasdipak99@gmail.com>
2019-02-04 00:17:34 +0530

    Versioning

---
+++ demo_rest_api/src/main/java/com/weservice/demo_rest_api/bean/PersonV1.java
@@ -0,0 +1,26 @@
+package com.weservice.demo_rest_api.bean;
+
+public class PersonV1 {
+	
+	private String name;
+	
+	public PersonV1() {
+		super();
+	}
+	
+	public PersonV1(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
---
+++ demo_rest_api/src/main/java/com/weservice/demo_rest_api/bean/PersonV2.java
@@ -0,0 +1,24 @@
+package com.weservice.demo_rest_api.bean;
+
+public class PersonV2 {
+	
+	private Name name;
+	
+	public PersonV2() {
+		super();
+	}
+	
+	public PersonV2(Name name) {
+		super();
+		this.name = name;
+	}
+
+	public Name getName() {
+		return name;
+	}
+
+	public void setName(Name name) {
+		this.name = name;
+	}
+
+}
